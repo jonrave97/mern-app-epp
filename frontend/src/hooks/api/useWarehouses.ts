e3224157491180751ad14f35f1dc4e8f3e78fe57
jonathan <jona_play_music@hotmail.com>
2025-11-19 21:48:07 -0300

    fix: modificando estructura de objeto a array para listar bodegas

--- frontend/src/hooks/api/useWarehouses.ts
+++ frontend/src/hooks/api/useWarehouses.ts
@@ -1,8 +1,16 @@
 import { useState, useEffect } from 'react';
 import { getWarehouses } from '../../services/warehouseService';
 
+interface Warehouse {
+  _id: string;
+  code: string;
+  name: string;
+  disabled: boolean;
+  __v: number;
+}
+
 export const useWarehouses = () => {
-  const [warehouses, setWarehouses] = useState([]);
+  const [warehouses, setWarehouses] = useState<Warehouse[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -10,10 +18,12 @@ export const useWarehouses = () => {
     const fetchWarehouses = async () => {
       try {
         const data = await getWarehouses();
-        if (data.success) {
-          setWarehouses(data.data);
-        } else {
-          setError(data.message);
+        console.log("📦 Estructura de data:", data);
+        if(Array.isArray(data))
+        {
+          setWarehouses(data);
+        }else{
+          setError('Error: Formato de datos inválido');
         }
       } catch (err) {
         const errorMessage = err instanceof Error ? err.message : 'Error desconocido';
