abfa92acf249d609723d01d750718f4b6c7d8aa5
jonathan <jona_play_music@hotmail.com>
2025-11-19 21:45:24 -0300

    update: modificacion de controlador para obtener todas las bodegas

--- backend/src/controllers/warehouseController.js
+++ backend/src/controllers/warehouseController.js
@@ -1,11 +1,14 @@
-import warehosemodels from '../models/warehouseModels.js';
+import warehosemodels from '../models/warehouseModel.js';
 
 export const getAllWarehouses = async (req, res) => {
   try {
     const warehouses = await warehosemodels.find(); // Obtener todos los almacenes
+    // console.log("✅ Almacenes obtenidos:", warehouses);
     return res.status(200).json(warehouses);
   } catch (error) {
     console.error("❌ Error al obtener almacenes:", error.message);
     return res.status(500).json({ message: "Error al obtener almacenes" });
   }
+  // console.log("Get all warehouses controller reached");
+  // return res.status(200).json({ message: "Get all warehouses controller reached" });
 };
