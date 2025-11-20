5f79bfe03adfc002ddcef36bc00f1eeb15bffaf1
jonathan <jona_play_music@hotmail.com>
2025-11-19 21:49:09 -0300

    fix: modifcando ruta api get /warehouse/all

--- frontend/src/services/warehouseService.ts
+++ frontend/src/services/warehouseService.ts
@@ -3,7 +3,8 @@ import API from './api';
 // Obtener todas las bodegas
 export const getWarehouses = async () => {
   try {
-    const response = await API.get('/warehouses');
+    const response = await API.get('/warehouses/all');
+    // console.log('Bodegas obtenidas:', response.data);
     return response.data;
   } catch (error) {
     console.error('Error al obtener bodegas:', error);
